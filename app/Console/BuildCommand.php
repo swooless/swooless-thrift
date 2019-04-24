17cfa6b4f182ac8332caf881ceedfee3fa7c5cb0
傅双亮 <shuangliang.fu@baozun.com>
2019-04-24 14:35:11 +0800

    refactor(console): 重构build命令的代码

--- app/Console/BuildCommand.php
+++ app/Console/BuildCommand.php
@@ -2,67 +2,78 @@
 
 namespace App\Console;
 
-use ArrayIterator;
 use Phar;
-use RecursiveDirectoryIterator;
+use Throwable;
+use ArrayIterator;
+use DI\NotFoundException;
+use DI\DependencyException;
 use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Throwable;
 
 class BuildCommand extends Command
 {
     protected function configure()
     {
         $this->setName('build')
-            ->setDescription('Build phar package!');
+            ->setDescription('Build phar package');
     }
 
     /**
      * @param InputInterface $input
      * @param OutputInterface $output
-     * @return void
+     * @return int
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         try {
             shell_exec("composer install --no-dev -d " . APP_ROOT);
-            $this->build($output);
+            $this->build();
             shell_exec("composer install -d " . APP_ROOT);
         } catch (Throwable $throwable) {
             $output->writeln("ERROR: {$throwable->getMessage()}");
+            return 254;
         }
+
+        return 0;
     }
 
-    private function build(OutputInterface $output)
+    /**
+     * @throws DependencyException
+     * @throws NotFoundException
+     */
+    private function build()
     {
-        $name = strtolower(app('SERVER_NAME'));
-        $file = APP_ROOT . "/dist/{$name}.phar";
-        if (is_file($file)) unlink($file);
+        $files = $this->filterFile();
+        $this->bale($files);
+    }
+
+
+    private function filterFile(): array
+    {
         $folder = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(APP_ROOT));
         $items = [];
 
-
         foreach ($folder as $item) {
-            //排除掉不需要的文件和目录
-            if ($this->jumpDir($item->getPathName())) {
+            $path = $item->getPathName();
+
+            //跳过不需要的文件和目录
+            if ($this->jumpDir($path)) {
                 continue;
             }
 
-            $filename = pathinfo($item->getPathName(), PATHINFO_BASENAME);
+            $filename = pathinfo($path, PATHINFO_BASENAME);
+
+            // 过滤隐藏文件
             if (substr($filename, 0, 1) != '.' || $filename == '.env') {
-                $items[substr($item->getPathName(), strlen(APP_ROOT))] = $item->getPathName();
-                $output->writeln("add file: $item");
+                $items[substr($path, strlen(APP_ROOT))] = $path;
+                echo "add file: $item \n";
             }
         }
 
-        $phar = new Phar($file);
-        $phar->startBuffering();
-        $phar->buildFromIterator(new ArrayIterator($items));
-        $phar->delete('phpunit.xml');
-        $phar->setDefaultStub("index.php");
-        $phar->stopBuffering();
+        return $items;
     }
 
     private function jumpDir($path): bool
@@ -76,5 +87,29 @@ class BuildCommand extends Command
         }
 
         return false;
+    }
+
+    /**
+     * 打包成 phar 包
+     * @param array $files
+     * @throws DependencyException
+     * @throws NotFoundException
+     */
+    private function bale(array $files)
+    {
+        $name = strtolower(app('SERVER_NAME'));
+        $file = APP_ROOT . "/dist/{$name}.phar";
+        if (is_file($file)) unlink($file);
+
+        $phar = new Phar($file);
+        $phar->startBuffering();
+        $phar->buildFromIterator(new ArrayIterator($files));
+        $phar->delete('phpunit.xml');
+        $phar->delete('composer.lock');
+        $phar->delete('composer.json');
+        $phar->delete('env-example');
+        $phar->delete('Dockerfile');
+        $phar->setDefaultStub("index.php");
+        $phar->stopBuffering();
     }
 }

f26efca8fbb7577aa591b2367c917778e70ebecb
傅双亮 <shuangliang.fu@baozun.com>
2019-04-25 13:15:35 +0800

    test(*): 添加单元测试以及案例和ci脚本

--- config/di.php
+++ config/di.php
@@ -1,19 +1,44 @@
 <?php declare(strict_types=1);
 
+use App\Handler\DemoHandler;
 use Swooless\Config\Loader\ZKLoader;
+use Swooless\Protocol\Demo\Constant;
+use Swooless\Protocol\Demo\ServerClient;
+use Swooless\Protocol\Demo\ServerProcessor;
 use Swooless\Registry\RedisRegistry;
 use Swooless\Registry\SPI;
 use Swooless\Config\Loader\LoaderInterface;
+use Thrift\Protocol\TBinaryProtocol;
+use Thrift\Transport\TFramedTransport;
+use Thrift\Transport\TSocket;
 
 use function DI\create;
 
 return [
-    'SERVER_NAME' => env('SERVER_NAME', 'demo'),
+    'SERVER_NAME' => function () {
+        Constant::get('SERVER_NAME');
+    },
 
-    'ServerProcessor' => create()
+    'ServerProcessor' => create(ServerProcessor::class)
         ->constructor(create(DemoHandler::class)),
 
     LoaderInterface::class => create(ZKLoader::class),
 
     SPI::class => create(RedisRegistry::class),
+
+    ServerClient::class => function () {
+        $timeout = 3;
+        /** @var RedisRegistry $registry */
+        $registry = app(SPI::class);
+
+        $node = $registry->getProvider((string)app('SERVER_NAME'));
+        $socket = new TSocket($node->getHost(), $node->getPort());
+        $socket->setSendTimeout($timeout * 1000);
+        $socket->setRecvTimeout($timeout * 1000);
+        $transport = new TFramedTransport($socket);
+        $protocol = new TBinaryProtocol($transport);
+        $client = new ServerClient($protocol);
+        $transport->open();
+        return $client;
+    }
 ];
